1766ad2865eedc8b30565a659571e5e06ba835ae
Yogesh <yogeshranwwa33@gmail.com>
2026-05-09 13:01:56 +0530

    Fixed API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://eventhive-backend-ekf5.onrender.com"
+  baseURL: "https://eventhive-backend-ekf5.onrender.com/api"
 });
 
 export default API;
